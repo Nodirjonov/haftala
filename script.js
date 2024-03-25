9c4575db7c1ec7114357212aceacaafb978e936c
Nodirjonov <sobirovakomila85@gmail.com>
2024-03-25 18:34:26 +0500

    haftalar

---
+++ script.js
@@ -0,0 +1,62 @@
+// let a = prompt('bukva kiriting A,B,C,D,E')
+// if(a=='A'){
+//     console.log('Siz alochisiz');
+// }
+// else if(a=='B'){
+//     console.log('Siz torchisiz');
+// }
+
+// else if(a=='C'){
+//     console.log('Siz yomon oqisiz');
+// }
+//  else if(a=='D'){
+//     console.log('Siz oqmisiz');
+// }
+// else if(a=='E'){
+//     console.log('Siz maktabdan haydalgansiz');
+// }else{
+//     console.log('Bu son yoq maktab bohosida');
+// }
+
+let a = prompt('1,2,3,4,5,6,7 bittasini kiriting')
+
+switch (a) {
+    case '1':
+        alert('Dushanba');
+        break;
+
+    case '2':
+        alert('Seyshanba');
+        break;
+
+    case '3':
+        alert('Chorshanba');
+        break;
+    case '4':
+        alert('Payshanba');
+        break;
+    case '5':
+        alert('Juma');
+        break;
+    case '6':
+        alert('Shanbi');
+        break;
+    case '7':
+        alert('Yakshanba');
+        break;
+  
+default:
+    alert('Hafta 7 kunli faqat');
+}
+
+
+// let u = Math.round(Math.random()*99+1)
+// alert(u)
+
+// switch (u) {
+//     case u%2==0:
+//         alert('Bu son 2 bolinadi');
+//         break;
+// }
+
+
